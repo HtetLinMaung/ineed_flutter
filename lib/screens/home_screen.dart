827fff8c30a00c5b4eeeb7aab3327d3836a30a9d
Htet Lin Maung <htet.lin.maung@fusionsol.com>
2020-09-07 13:49:39 +0630

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ineed_flutter/components/container/absolute_container.dart';
+import 'package:ineed_flutter/constants/colors.dart';
+import 'package:ineed_flutter/store/app_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = 'HomeScreen';
@@ -11,7 +14,38 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: AbsoluteContainer(
           child: Column(
-            children: [],
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 15,
+                  top: 10,
+                ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 25,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.menu,
+                      size: 22,
+                      color: kLabelColor,
+                    ),
+                    context.watch<AppProvider>().profileImage.isEmpty
+                        ? CircleAvatar(
+                            radius: 11,
+                            backgroundImage: AssetImage(
+                                'assets/images/avatar-placeholder.webp'),
+                          )
+                        : CircleAvatar(
+                            radius: 11,
+                            backgroundImage: NetworkImage(
+                                context.watch<AppProvider>().profileImage),
+                          )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
